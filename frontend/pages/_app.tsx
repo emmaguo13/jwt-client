d4208e892036bca7d73b0343e1a74136e62a3177
emmaguo13 <emmaguo@berkeley.edu>
2023-06-27 05:11:22 -0400

    starter app

---
+++ frontend/pages/_app.tsx
@@ -0,0 +1,12 @@
+import type { AppProps } from "next/app"
+
+// Use of the <SessionProvider> is mandatory to allow components that call
+// `useSession()` anywhere in your application to access the `session` object.
+export default function App({
+  Component,
+  pageProps: {...pageProps },
+}: AppProps<{}>) {
+  return (
+      <Component {...pageProps} />
+  )
+}
